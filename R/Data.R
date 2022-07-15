9a9a7f3e8b7a5dcd9b9e0c4468565d2098930330
TamTobiRalph <ralphtamtobi@hotmail.com>
2022-07-15 19:15:48 +0100

    Updated or documentations

--- R/Data.R
+++ R/Data.R
@@ -46,6 +46,84 @@
 
 "england_nhs_ambulance_quality_indicators"
 
+#' Bed Availability and Occupancy
+#'
+#' This data set contains the  number of available bed days & night. 
+#' It gives the  number of occupied bed days by consultant main specialty.
+#'
+#' @format A tibble data frame with 212 rows and 22 variables:
+#' \describe{
+#'   \item{% trust_code}{Abbreviation for trust name}
+#'   \item{% trust_name}{Trust Name}
+#'   \item{% Total_Day_Beds_Occupied}{Beds occupied durring the day}
+#'   \item{General_Acute_Day_Beds_Occupied}{Beds Occupied durring the day in General_Acute unit}
+#'   \item{Learning Disabilities Day Beds Occupied}{Beds Occupied durring the day in Learning Disabilities unit}
+#'   \item{Maternity Night day Occupied}{Beds Occupied durring the day in the Maternity unit}
+#'   \item{Mental Illness Day Beds Occupied}{Beds Occupied durring the day in Mental Illness unit}
+#'   \item{% Total Day Beds Occupied }{Total Beds Occupied durring the day }
+#'   \item{% General_Acute_Day_Beds_Occupied}{Beds Occupied durring the day in General_Acute unit}
+#'   \item{% Learning Disabilities Day Beds Occupied}{Beds Occupied durring the day in Learning Disabilities unit}
+#'   \item{% Maternity day Occupied}{Beds Occupied durring the day in the Maternity unit}
+#'   \item{% Mental Illness Day Beds Occupied}{Beds Occupied durring the day in Mental Illness unit}
+#'   \item{% Total Night Beds Occupied}{Total Night Beds Occupied}
+#'   \item{General Acute Night Beds Occupied}{Beds Occupied durring the night in General_Acute_unit}
+#'   \item{Learning Disabilities Night Beds Occupied}{Beds Occupied durring the night in Learning Disabilities unit}
+#'   \item{Maternity Night Beds Occupied}{Beds Occupied durring the night in the Maternity unit}
+#'   \item{Mental Illness Night Beds Occupied}{Mental Illness Night Beds Occupied}
+#'   \item{% Total Night Beds Occupied}{Total Night Beds Occupied}
+#'   \item{% General Acute Night Beds Occupied}{Beds Occupied durring the night in General_Acute_unit}
+#'   \item{% Learning Disabilities Night Beds Occupied}{Beds Occupied durring the night in Learning Disabilities unit}
+#'   \item{% Maternity Night Beds Occupied}{Beds Occupied durring the night in the Maternity unit}
+#'   \item{% Mental Illness Night Beds Occupied}{Beds Occupied durring the night in Mental Illness unit}
+#'   
+#' 
+#' }
+#' @source \url{https://www.england.nhs.uk/statistics/statistical-work-areas/ambulance-quality-indicators/ambulance-quality-indicators-data-2022-23/}
+
+"england_nhs_bed_occupancy"
+
+#' Cancer Waiting Times
+#'
+#' This data set contains the waiting times of people referred by GP with suspected cancer or 
+#' breast symptoms and those subsequently diagnosed with and treated for cancer by the NHS in England.
+#'
+#' @format A tibble data frame with 1400 rows and 6 variables:
+#' \describe{
+#'   \item{% trust_code}{Abbreviation for trust name}
+#'   \item{% trust_name}{Trust Name}
+#'   \item{Standard}{ST}
+#'   \item{Total Treated}{Total number of patients treated}
+#'   \item{Within Standard}{WS}
+#'   \item{Breaches}{Breaches}
+#'   ...
+#' 
+#' }
+#' @source \url{https://www.england.nhs.uk/statistics/statistical-work-areas/cancer-waiting-times/}
+
+"england_nhs_cancer_wait_times"
+
+
+#' Cancer Waiting Times
+#'
+#' This data set contains the waiting times of people referred by GP with suspected cancer or 
+#' breast symptoms and those subsequently diagnosed with and treated for cancer by the NHS in England.
+#'
+#' @format A tibble data frame with 212 rows and 7 variables:
+#' \describe{
+#'   \item{% trust_code}{Abbreviation for trust name}
+#'   \item{% trust_name}{Trust Name}
+#'   \item{Waiting List Total}{Waiting list of patients}
+#'   \item{Waiting 6+ weeks}{Waiting list of patients for 6 Weeks plus}
+#'   \item{Waiting 13+ weeks}{Waiting list of patients for 6 Weeks plus}
+#'   \item{% Waiting 6+ weeks}{Total number Waiting list of patients for 6 Weeks plus}
+#'   \item{% Waiting 13+ weeks}{Total number Waiting list of patients for 13 Weeks plus}
+#'   ...
+#' 
+#' }
+#' @source \url{https://www.england.nhs.uk/statistics/statistical-work-areas/diagnostics-waiting-times-and-activity/monthly-diagnostics-waiting-times-and-activity/monthly-diagnostics-data-2022-23/}
+
+"england_nhs_diagnostic_wait_times"
+
 
 
 
